87e0b2c9a1b86014271f5f6478231bc588084595
meesalji <156134165+meesalji@users.noreply.github.com>
2024-02-14 18:32:22 -0500

    Update email.js

--- email.js
+++ email.js
@@ -1,1 +1,12 @@
-
+var shown = false;
+function showhideEmail(){
+	if (shown){
+		document.getElementById('email').innerHTML = "Show my email";
+		shown = false;
+	}else{
+		var myemail = "<a href='mailto:meesalji"+   "@" +
+		"ucmail.uc.edu'>meesalji" + "@" + "ucmail.uc.edu</a>";
+		document.getElementById('email').innerHTML= myemail;
+		shown=true;
+	}
+}
